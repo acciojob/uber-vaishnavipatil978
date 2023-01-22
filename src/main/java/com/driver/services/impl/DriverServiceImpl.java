429dd3765d9cdf0899b42f70cbdd184ab904605d
vaishnavi <vaishnavipatil978@gmail.com>
2023-01-22 18:31:44 +0530

    Commit 1

--- src/main/java/com/driver/services/impl/DriverServiceImpl.java
+++ src/main/java/com/driver/services/impl/DriverServiceImpl.java
@@ -22,17 +22,33 @@ public class DriverServiceImpl implements DriverService {
 	public void register(String mobile, String password){
 		//Save a driver in the database having given details and a cab with ratePerKm as 10 and availability as True by default.
 
+		try{
+			Driver driver= new Driver(mobile,password);
+			Cab cab= new Cab(10,true);
+
+			cab.setDriver(driver);
+			driver.setCab(cab);
+
+			driverRepository3.save(driver);
+		}
+		catch(Exception e){
+			return;
+		}
 	}
 
 	@Override
 	public void removeDriver(int driverId){
 		// Delete driver without using deleteById function
 
+		driverRepository3.deleteById(driverId);
 	}
 
 	@Override
 	public void updateStatus(int driverId){
 		//Set the status of respective car to unavailable
 
+		Driver driver = driverRepository3.findById(driverId).get();
+		driver.getCab().setAvailable(false);
+		driverRepository3.save(driver);
 	}
 }
